3c5abd70bbb8ac54a41ecf56c45c0737755b5bbb
Roy <info@roygerritse.nl>
2025-07-24 00:44:35 +0200

    Some XML serialization

--- src/MIM.Schema.Tests/UnitTest1.cs
+++ src/MIM.Schema.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
-﻿namespace MIM.Schema.Tests;
+﻿using System.Xml.Serialization;
+
+namespace MIM.Schema.Tests;
 
 public class Tests
 {
@@ -10,6 +12,28 @@ public class Tests
     [Test]
     public void Test1()
     {
+        var informatieModel = new Informatiemodel
+        {
+            naam = "test",
+            MIMVersie = InformatiemodelMIMVersie.Item111
+        };
+        
+        
+        XmlSerializer serializer = new XmlSerializer(typeof(Informatiemodel));
+
+        // Serialize the object to a string
+        using (StringWriter writer = new StringWriter())
+        {
+            serializer.Serialize(writer, informatieModel);
+            string xmlString = writer.ToString();
+            Console.WriteLine(xmlString);
+        }
+
+        // Serialize the object to a file
+        using (FileStream file = File.Create("informatiemodel.xml"))
+        {
+            serializer.Serialize(file, informatieModel);
+        }
         Assert.Pass();
     }
 }
